1c02f5cc964c07acb49f8e699fbab1b44e582f6d
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-09-19 10:54:56 -0400

    cors fix 4

--- src/api/index.ts
+++ src/api/index.ts
@@ -31,7 +31,7 @@ app.use(
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin!)) {
-    res.setHeader("Access-Control-Allow-Origin", origin!);
+    res.setHeader("Access-Control-Allow-Origin", "*");
   }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
