9c1f824cc582c952b576074e77677c217a569521
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-09-19 07:28:18 -0400

    chore: ensure tsc and build tools installed for CI

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import syllabiRouter from "./routes/syllabiRoutes.js";
+import classRouter from "./routes/classRoutes.js";
+import assignmentsRouter from "./routes/assignmentsRoutes.js";
+import googleRouter from "./routes/googleRoutes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(classRouter);
+app.use(syllabiRouter);
+app.use(assignmentsRouter);
+app.use(googleRouter);
+
+app.listen(4000);
