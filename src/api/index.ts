531acd82be15fd867c4149688e411d67db3e99f1
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-09-19 10:40:29 -0400

    cors fix 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,17 +8,44 @@ import googleRouter from "../routes/googleRoutes.js";
 const app = express();
 app.use(cors());
 
+const FRONTEND_URL = "https://law-bandit-front-end.vercel.app";
+const DEV_ORIGIN = "http://localhost:5173";
+const allowedOrigins = [FRONTEND_URL, DEV_ORIGIN];
+
 app.use(
   cors({
-    origin: "https://law-bandit-syllabus.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS policy: origin not allowed"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true,
   })
 );
 
-app.use(express.json());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin!)) {
+    res.setHeader("Access-Control-Allow-Origin", origin!);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-app.options("*", cors());
+app.use(express.json());
 
 app.use(classRouter);
 app.use(syllabiRouter);
