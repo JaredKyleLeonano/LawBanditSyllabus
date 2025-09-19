3222f7e4a5d90c3813d45609fb3f3426f9e0146c
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-09-19 18:31:58 -0400

    praying again

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 import serverless from "serverless-http";
 
-import app from "../dist/api/index.js";
+let handler = null;
 
-export default serverless(app);
+export default async function (req, res) {
+  console.log("wrapper invoked");
+  try {
+    if (!handler) {
+      console.log("dynamic import starting");
+      const mod = await import("../dist/api/index.js");
+      const app = mod.default || mod.app || mod;
+      handler = serverless(app);
+      console.log("handler created");
+    }
+    return handler(req, res);
+  } catch (err) {
+    console.error("INIT ERROR", err);
+    res.statusCode = 500;
+    return res.end("init error");
+  }
+}
