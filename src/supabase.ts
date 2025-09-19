f5df49e95823510da8429b7e0d3c3eb77c79971d
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-09-19 18:49:49 -0400

    revert hard code

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,9 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl: string = "https://pjiwxqbdwebgzzjzjmkb.supabase.co";
-const supabaseKey: string =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqaXd4cWJkd2ViZ3p6anpqbWtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMDMwNTEsImV4cCI6MjA3MjY3OTA1MX0.XcdilLjGBc9owScIE79RcMoYRKFe5OTk7MJQLwDFHRk";
+const supabaseUrl: string = process.env.SUPABASE_URL!;
+const supabaseKey: string = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = (sessionToken: string) => {
   return createClient(supabaseUrl, supabaseKey, {
